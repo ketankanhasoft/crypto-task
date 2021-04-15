7c3ba9e3fe3b4820686814cb329d964a6c55c84b
ketankanhasoft <ketan.kanhasoft@gmail.com>
2021-04-15 11:25:32 +0530

    add pep8 format

--- crypto/cryptoAPI/utils/scrape.py
+++ crypto/cryptoAPI/utils/scrape.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
+
 class Scrape:
 
     def __init__(self):
@@ -12,15 +13,12 @@ class Scrape:
             req = Request('https://www.theblockcrypto.com/', headers={'User-Agent': 'Mozilla/5.0'})
             html = urlopen(req).read()
             bs = BeautifulSoup(html, 'html.parser')
-            
+
             storyfeed = bs.find("div", class_="storyFeed").find_all("article")
-            
             for story in storyfeed:
                 title = story.find("div", class_="align-items-start").h3.text
                 description = story.find("div", class_="align-items-start").div.text
-                
                 news_list.append({"title": title, "description": description})
-            
             return news_list
         except Exception as e:
             return news_list
--- crypto/cryptoAPI/views/ethereum_view.py
+++ crypto/cryptoAPI/views/ethereum_view.py
@@ -4,8 +4,9 @@ from rest_framework.response import Response
 from cryptoAPI.utils import scrape
 from cryptoAPI.serializers import NewsSerializer
 
+
 class EthereumPrice(APIView):
 
     def get(self, request, *args, **kwargs):
         ethereum_price = scrape.Scrape().scrape_ethereum_price()
-        return Response(status=200, data= {"price": ethereum_price})
+        return Response(status=200, data={"price": ethereum_price})
