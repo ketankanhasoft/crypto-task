8188d89d584d5c9f3c70b9e66937ad048cd9fd7e
ketankanhasoft <ketan.kanhasoft@gmail.com>
2021-04-15 15:42:07 +0530

    Add CORS allow for call api from FE

--- crypto/crypto/settings.py
+++ crypto/crypto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Thirdparty apps
+    "corsheaders",
     # LOCAL APPS #
     "cryptoAPI",
 ]
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "crypto.urls"
@@ -125,3 +128,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL = True
